67d776c694b2919dfb81456c12da3e6677afe5b7
kalashshah <202051096@iiitvadodara.ac.in>
2023-05-17 23:44:02 +0530

    :arrow_up: contract revamp

--- hardhat/contracts/KitaiPay.sol
+++ hardhat/contracts/KitaiPay.sol
@@ -5,156 +5,376 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 error KitaiPay__PaymentAlreadyCompleted();
 error KitaiPay__PaymentAlreadyCancelled();
-error KitaiPay__EthAmountMismatch();
-error KitaiPay__TokenAmountMismatch();
-error KitaiPay__EmptyTokenArray();
-error KitaiPay__InvalidInput();
-error KitaiPay__OnlyOwner();
+error KitaiPay__OnlyContractOwnerAllowed();
+error KitaiPay__OnlyPaymentOwnerAllowed();
+error KitaiPay__NotEnoughBalance();
+error KitaiPay__NotASenderOfThisPayment();
+error KitaiPay__ShareAlreadyAdded();
+error KitaiPay__ShareNotAdded();
+error KitaiPay__InvalidSignature();
+error KitaiPay__InvalidPaymentId();
+error KitaiPay__InternalContractError();
 
-// NOTE: TokenDetails has unique token addresses
-// NOTE: User addresses inside TokenDetails are not unique
-
-struct TokenSender {
+struct PaymentInputUnit {
     address user;
-    uint256 amount;
-    bool hasCompleted;
-}
-struct TokenReceiver {
-    address user;
+    address token;
     uint256 amount;
 }
 
-struct TokenDetails {
-    address tokenAddress;
-    TokenSender[] senders;
-    TokenReceiver[] receivers;
+struct CreatePaymentInput {
+    PaymentInputUnit[] senders;
+    PaymentInputUnit[] receivers;
+}
+
+struct PaymentItem {
+    address token;
+    uint256 amount;
 }
 
 struct Payment {
-    TokenDetails[] tokenDetails;
     address owner;
     uint256 paymentId;
     string description;
     bool completed;
     bool cancelled;
+    uint256 paymentsCompleted;
 }
 
 contract KitaiPay {
-    Payment[] private _payments;
+    event PaymentCreated(uint256 indexed paymentId, address indexed owner);
+    event PaymentCompleted(uint256 indexed paymentId);
+    event PaymentCancelled(uint256 indexed paymentId);
+    event PaymentShareAdded(uint256 indexed paymentId, address indexed user);
+    event PaymentShareRemoved(uint256 indexed paymentId, address indexed user);
 
-    function hasDuplicates(
-        TokenDetails[] memory tokenDetails
-    ) internal pure returns (bool) {
-        for (uint256 i = 0; i < tokenDetails.length; i++) {
-            for (uint256 j = i + 1; j < tokenDetails.length; j++) {
-                if (
-                    tokenDetails[i].tokenAddress == tokenDetails[j].tokenAddress
-                ) return true;
-            }
-        }
-        return false;
+    // @dev The owner of the contract.
+    address private immutable _owner;
+
+    // @dev The payment data, paymentId => Payment
+    mapping(uint256 => Payment) private _payments;
+
+    // @dev The payment signature, paymentId => bytes32
+    mapping(uint256 => bytes32) private _paymentSignature;
+
+    // @dev The payment senders, paymentId => address[]
+    mapping(uint256 => address[]) private _paymentSenders;
+
+    // @dev The payment sender items, paymentId => sender address => PaymentItem[]
+    mapping(uint256 => mapping(address => PaymentItem[]))
+        private _paymentSenderItems;
+
+    // @dev The payment sender statuses, paymentId => sender address => bool
+    mapping(uint256 => mapping(address => bool)) private _paymentSenderStatuses;
+
+    // @dev The payment receivers, paymentId => address[]
+    mapping(uint256 => address[]) private _paymentReceivers;
+
+    // @dev The payment receiver items, paymentId => receiver address => PaymentItem[]
+    mapping(uint256 => mapping(address => PaymentItem[]))
+        private _paymentReceiverItems;
+
+    /*
+     * @dev The constructor sets the owner of the contract to the sender account.
+     */
+    constructor() {
+        _owner = msg.sender;
     }
 
-    function checkTokenAmounts(
-        TokenDetails[] memory tokenDetails
-    ) internal pure returns (bool) {
-        if (tokenDetails.length == 0) revert KitaiPay__EmptyTokenArray();
-        TokenDetails memory ethDetails = tokenDetails[0];
-        if (ethDetails.tokenAddress != address(0))
-            revert KitaiPay__InvalidInput();
-        for (uint256 i = 0; i < tokenDetails.length; i++) {
-            TokenDetails memory tokenDetail = tokenDetails[i];
-            uint256 totalTokenSent = 0;
-            uint256 totalTokenReceived = 0;
-            for (uint256 j = 0; j < tokenDetail.senders.length; j++) {
-                totalTokenSent += tokenDetail.senders[j].amount;
-            }
-            for (uint256 j = 0; j < tokenDetail.receivers.length; j++) {
-                totalTokenReceived += tokenDetail.receivers[j].amount;
-            }
-            if (totalTokenSent != totalTokenReceived) {
-                return false;
-            }
+    /*
+     * @dev Checks if the hash generated from the payment data is valid.
+     * @param paymentId The payment id.
+     * @param input The payment data input.
+     */
+    function ensureHashValidity(
+        uint256 paymentId,
+        CreatePaymentInput memory input
+    ) public view {
+        bytes32 generatedHash = keccak256(abi.encodePacked(paymentId));
+        for (uint256 i = 0; i < input.senders.length; i++) {
+            generatedHash = keccak256(
+                abi.encodePacked(
+                    generatedHash,
+                    input.senders[i].user,
+                    input.senders[i].token,
+                    input.senders[i].amount
+                )
+            );
         }
-        return true;
+        for (uint256 i = 0; i < input.receivers.length; i++) {
+            generatedHash = keccak256(
+                abi.encodePacked(
+                    generatedHash,
+                    input.receivers[i].user,
+                    input.receivers[i].token,
+                    input.receivers[i].amount
+                )
+            );
+        }
+        if (generatedHash != _paymentSignature[paymentId]) {
+            revert KitaiPay__InvalidSignature();
+        }
     }
 
+    /*
+     * @dev Checks if the sender is the owner of the contract.
+     */
+    function ensureIsOwner() internal view {
+        if (msg.sender != _owner) {
+            revert KitaiPay__OnlyContractOwnerAllowed();
+        }
+    }
+
+    /*
+     * @dev Checks if the sender is the owner of the payment.
+     * @param paymentId The payment id.
+     */
+    function ensureIsPaymentOwner(uint256 paymentId) internal view {
+        if (msg.sender != _payments[paymentId].owner) {
+            revert KitaiPay__OnlyPaymentOwnerAllowed();
+        }
+    }
+
+    /*
+     * @dev Checks if the payment is valid, not completed and not cancelled.
+     * @param paymentId The payment id.
+     */
+    function ensureIsValidPayment(uint256 paymentId) internal view {
+        if (_paymentSenders[paymentId].length == 0) {
+            revert KitaiPay__InvalidPaymentId();
+        }
+        if (_payments[paymentId].completed) {
+            revert KitaiPay__PaymentAlreadyCompleted();
+        }
+        if (_payments[paymentId].cancelled) {
+            revert KitaiPay__PaymentAlreadyCancelled();
+        }
+    }
+
+    /*
+     * @dev Checks if the sender is a sender of the payment.
+     * @param paymentId The payment id.
+     */
+    function ensureIsSender(uint256 paymentId) internal view {
+        if (_paymentSenderItems[paymentId][msg.sender].length == 0) {
+            revert KitaiPay__NotASenderOfThisPayment();
+        }
+    }
+
+    /*
+     * @dev Creates a new payment if the hash generated from the payment data is valid.
+     * @param paymentId The payment id.
+     * @param input The payment data input.
+     * @param description The payment description.
+     * @param hasAddedShare The payer has added a share.
+     */
     function createPayment(
-        TokenDetails[] memory tokenDetails,
-        string memory description
+        uint256 paymentId,
+        CreatePaymentInput memory input,
+        string memory description,
+        bool hasAddedShare
     ) public payable {
-        if (!checkTokenAmounts(tokenDetails))
-            revert KitaiPay__TokenAmountMismatch();
+        ensureHashValidity(paymentId, input);
 
-        // TODO: Check if user's share was sent along with the transaction
+        for (uint256 i = 0; i < input.senders.length; i++) {
+            _paymentSenders[paymentId].push(input.senders[i].user);
+            _paymentSenderItems[paymentId][input.senders[i].user].push(
+                PaymentItem({
+                    token: input.senders[i].token,
+                    amount: input.senders[i].amount
+                })
+            );
+        }
 
-        if (hasDuplicates(tokenDetails)) revert KitaiPay__InvalidInput();
+        for (uint256 i = 0; i < input.receivers.length; i++) {
+            _paymentReceivers[paymentId].push(input.receivers[i].user);
+            _paymentReceiverItems[paymentId][input.receivers[i].user].push(
+                PaymentItem({
+                    token: input.receivers[i].token,
+                    amount: input.receivers[i].amount
+                })
+            );
+        }
 
-        Payment storage createdPayment = _payments.push();
-        createdPayment.cancelled = false;
-        createdPayment.completed = false;
-        createdPayment.description = description;
-        createdPayment.owner = msg.sender;
-        createdPayment.paymentId = _payments.length - 1;
+        _payments[paymentId] = Payment({
+            owner: msg.sender,
+            paymentId: paymentId,
+            description: description,
+            completed: false,
+            cancelled: false,
+            paymentsCompleted: 0
+        });
 
-        for (uint256 i = 0; i < tokenDetails.length; i++) {
-            TokenDetails storage createdTokenDetail = createdPayment
-                .tokenDetails
-                .push();
-            createdTokenDetail.tokenAddress = tokenDetails[i].tokenAddress;
-            for (uint256 j = 0; j < tokenDetails[i].senders.length; j++) {
-                TokenSender storage createdSender = createdTokenDetail
-                    .senders
-                    .push();
-                createdSender.user = tokenDetails[i].senders[j].user;
-                createdSender.amount = tokenDetails[i].senders[j].amount;
-                if (createdSender.amount == 0) revert KitaiPay__InvalidInput();
-                createdSender.hasCompleted = false;
+        if (hasAddedShare) {
+            addUserShare(paymentId);
+        }
+
+        emit PaymentCreated(paymentId, msg.sender);
+    }
+
+    /*
+     * @dev Adds a sender's share to the payment.
+     * @param paymentId The payment id.
+     */
+    function addUserShare(uint256 paymentId) public payable {
+        ensureIsValidPayment(paymentId);
+        ensureIsSender(paymentId);
+
+        if (_paymentSenderStatuses[paymentId][msg.sender]) {
+            revert KitaiPay__ShareAlreadyAdded();
+        }
+
+        PaymentItem[] memory senderItems = _paymentSenderItems[paymentId][
+            msg.sender
+        ];
+        if (msg.value < senderItems[0].amount) {
+            revert KitaiPay__NotEnoughBalance();
+        }
+
+        payable(address(this)).transfer(senderItems[0].amount);
+        for (uint256 i = 1; i < senderItems.length; i++) {
+            IERC20 token = IERC20(senderItems[i].token);
+            if (
+                !token.transferFrom(
+                    msg.sender,
+                    address(this),
+                    senderItems[i].amount
+                )
+            ) {
+                revert KitaiPay__NotEnoughBalance();
             }
-            for (uint256 j = 0; j < tokenDetails[i].receivers.length; j++) {
-                TokenReceiver storage createdReceiver = createdTokenDetail
-                    .receivers
-                    .push();
-                createdReceiver.user = tokenDetails[i].receivers[j].user;
-                createdReceiver.amount = tokenDetails[i].receivers[j].amount;
-                if (createdReceiver.amount == 0)
-                    revert KitaiPay__InvalidInput();
+        }
+
+        _paymentSenderStatuses[paymentId][msg.sender] = true;
+        _payments[paymentId].paymentsCompleted += 1;
+
+        if (
+            _payments[paymentId].paymentsCompleted ==
+            _paymentSenders[paymentId].length
+        ) {
+            sendReceiversShare(paymentId);
+            _payments[paymentId].completed = true;
+
+            emit PaymentCompleted(paymentId);
+        }
+
+        emit PaymentShareAdded(paymentId, msg.sender);
+    }
+
+    /*
+     * @dev Sends the user's share to the back to the user.
+     * @param paymentId The payment id.
+     */
+    function returnUserShare(uint256 paymentId) public {
+        ensureIsValidPayment(paymentId);
+        ensureIsSender(paymentId);
+
+        if (!_paymentSenderStatuses[paymentId][msg.sender]) {
+            revert KitaiPay__ShareNotAdded();
+        }
+
+        PaymentItem[] memory senderItems = _paymentSenderItems[paymentId][
+            msg.sender
+        ];
+        payable(msg.sender).transfer(senderItems[0].amount);
+        for (uint256 i = 1; i < senderItems.length; i++) {
+            IERC20 token = IERC20(senderItems[i].token);
+            if (
+                !token.transferFrom(
+                    address(this),
+                    msg.sender,
+                    senderItems[i].amount
+                )
+            ) {
+                revert KitaiPay__InternalContractError();
+            }
+        }
+
+        _paymentSenderStatuses[paymentId][msg.sender] = false;
+        _payments[paymentId].paymentsCompleted -= 1;
+
+        emit PaymentShareRemoved(paymentId, msg.sender);
+    }
+
+    /*
+     * @dev Sends the receivers' share to the receivers and completes the payment.
+     * @param paymentId The payment id.
+     */
+    function sendReceiversShare(uint256 paymentId) public {
+        ensureIsValidPayment(paymentId);
+        address[] memory receivers = _paymentReceivers[paymentId];
+        for (uint256 i = 0; i < receivers.length; i++) {
+            PaymentItem[] memory receiverItems = _paymentReceiverItems[
+                paymentId
+            ][receivers[i]];
+            payable(receivers[i]).transfer(receiverItems[0].amount);
+            for (uint256 j = 1; j < receiverItems.length; j++) {
+                IERC20 token = IERC20(receiverItems[j].token);
+                if (
+                    !token.transferFrom(
+                        address(this),
+                        receivers[i],
+                        receiverItems[j].amount
+                    )
+                ) {
+                    revert KitaiPay__InternalContractError();
+                }
             }
         }
     }
 
+    /*
+     * @dev Cancels a payment.
+     * @param paymentId The payment id.
+     */
     function cancelPayment(uint256 paymentId) public {
-        Payment storage payment = _payments[paymentId];
-        if (payment.completed) {
-            revert KitaiPay__PaymentAlreadyCompleted();
+        ensureIsValidPayment(paymentId);
+        ensureIsPaymentOwner(paymentId);
+
+        if (_paymentSenderStatuses[paymentId][msg.sender]) {
+            revert KitaiPay__ShareAlreadyAdded();
         }
-        if (payment.cancelled) {
-            revert KitaiPay__PaymentAlreadyCancelled();
+
+        _payments[paymentId].cancelled = true;
+
+        for (uint256 i = 0; i < _paymentSenders[paymentId].length; i++) {
+            address sender = _paymentSenders[paymentId][i];
+            if (!_paymentSenderStatuses[paymentId][sender]) {
+                continue;
+            }
+            PaymentItem[] memory senderItems = _paymentSenderItems[paymentId][
+                sender
+            ];
+            payable(sender).transfer(senderItems[0].amount);
+            for (uint256 j = 1; j < senderItems.length; j++) {
+                IERC20 token = IERC20(senderItems[j].token);
+                if (
+                    !token.transferFrom(
+                        address(this),
+                        sender,
+                        senderItems[j].amount
+                    )
+                ) {
+                    revert KitaiPay__InternalContractError();
+                }
+            }
         }
-        if (payment.owner != msg.sender) {
-            revert KitaiPay__OnlyOwner();
-        }
-        payment.cancelled = true;
-        // TODO: refund
+
+        emit PaymentCancelled(paymentId);
     }
 
-    function addShareToPayment(uint256 paymentId) public payable {
-        Payment storage payment = _payments[paymentId];
-        if (payment.completed) {
-            revert KitaiPay__PaymentAlreadyCompleted();
-        }
-        if (payment.cancelled) {
-            revert KitaiPay__PaymentAlreadyCancelled();
-        }
-        // TODO: check if user has completed his share
-        // TODO: add share to payment
-        // TODO: check if payment is completed
-        // TODO: if completed, transfer tokens to receivers
-    }
+    /*
+     * @dev Adds a signature to a payment.
+     * @param paymentId The payment id.
+     * @param signature The signature.
+     */
+    function addPaymentSignature(uint256 paymentId, bytes32 signature) public {
+        ensureIsValidPayment(paymentId);
+        ensureIsOwner();
 
-    function getPayment(
-        uint256 paymentId
-    ) public view returns (Payment memory) {
-        return _payments[paymentId];
+        if (_paymentSignature[paymentId] != 0x0) {
+            revert KitaiPay__InvalidPaymentId();
+        }
+
+        _paymentSignature[paymentId] = signature;
     }
 }
